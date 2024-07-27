5095122e03d34ee5043f4159b5b5bea02fd796e7
Balalomba Barandao <barand14@gmail.com>
2024-07-26 20:34:29 -0400

    package.json and config.js modified

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header() {
+  return (
+    <header>
+      <h1 className="header__title">Balalomba Barandao's Portfolio</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <NavLink exact to="/" activeClassName="nav-link active" className="nav-link">About Me</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/portfolio" activeClassName="nav-link active" className="nav-link">Portfolio</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" activeClassName="nav-link active" className="nav-link">Contact</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/resume" activeClassName="nav-link active" className="nav-link">Resume</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
