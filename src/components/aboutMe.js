470cc35d1d0cd1d7a2b4756c311e580bd04027f7
Balalomba Barandao <barand14@gmail.com>
2024-07-27 21:35:14 -0400

    apps added

---
+++ src/components/aboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="./images/profilepicture.jpg" alt="Balalomba Barandao" />
+      <p>Extremely down to earth, I try my best to get along with people or most people, to be concise.
+                I'm a big fan of sport in general and soccer specifically.
+                I also love science and technology which have led my curiosity to this bootcamp. With no coding
+                background, I accepted my inner call to explore this field completely strange to my knowledge.
+                More than half way through this bootcamp, I've learned quite a lot and to be honest , it has been and continue to be a huge challenge. </p>
+    </section>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/portofolio.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Project from './Project';
+
+function Portfolio() {
+  const projects = [
+    {
+      title: 'MarvelVerse-Central',
+      deployedLink: 'https://skylerrhys.github.io/MarvelVerse-Central/',
+      githubLink: 'https://github.com/SkylerRhys/MarvelVerse-Central'
+    },
+    
+    {
+        title:'Triviosa',
+        deployedLink:'https://drsaho.github.io/Triviosa/',
+        githubLink: 'https://github.com/drsaho/Triviosa'
+    },
+
+  ];
+
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      {projects.map((project, index) => (
+        <Project key={index} {...project} />
+      ))}
+    </section>
+  );
+}
+
+export default Portfolio;
