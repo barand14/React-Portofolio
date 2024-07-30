046c97820095b914060dea395656787f55575b88
Balalomba Barandao <barand14@gmail.com>
2024-07-30 14:25:45 -0400

    test deploy

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,19 +1,29 @@
-import Project from './Project';
+import Project from './project.jsx';
 
 function Portfolio() {
   const projects = [
     {
       title: 'MarvelVerse-Central',
+      image: "https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk3MzM3ODU4NTU2NTY4Nzc1/marveldisney.jpg",
+      description:"Project 1 description",
       deployedLink: 'https://skylerrhys.github.io/MarvelVerse-Central/',
       githubLink: 'https://github.com/SkylerRhys/MarvelVerse-Central'
     },
     
     {
         title:'Triviosa',
+        image: "imageurl",
+      description:"Project 2 description",
         deployedLink:'https://drsaho.github.io/Triviosa/',
         githubLink: 'https://github.com/drsaho/Triviosa'
     },
-
+    {
+      title:'project 3',
+      image: "imageurl",
+    description:"Project 3 description",
+      deployedLink:'https://drsaho.github.io/Triviosa/',
+      githubLink: 'https://github.com/drsaho/Triviosa'
+  },
   ];
 
   return (
---
+++ src/components/resume.jsx
@@ -0,0 +1,22 @@
+import resume from "../assets/example.png"
+function Resume(){
+    return(
+        <div>
+          <h2>Download my <a href={resume} download>resume</a></h2>
+            <ul>
+                <h3>Front End</h3>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+            </ul>
+            <ul>
+                <h3>Back End</h3>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Resume
